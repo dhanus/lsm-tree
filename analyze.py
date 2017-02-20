f12cef97d09972886010c4e12f7f095043f010f9
dhanus <dhanus@mit.edu>
2017-02-19 22:05:31 -0500

    analyzes data for one data size and buffer size

---
+++ analyze.py
@@ -0,0 +1,23 @@
+import numpy as np 
+
+actions = ['put'] 
+data_sizes = ['1000', '10000', '100000', '1000000', '10000000']
+buffer_sizes = ['100', '1000', '10000', '100000']  
+
+stats = {} 
+
+a = actions[0]
+ds = data_sizes[0]
+bs = buffer_sizes[0]
+
+id = '%s_%s_%s'%(a, ds, bs)
+
+f = open('data/%s.txt'%(id), 'r')
+data = f.readline().split(',') [:-1] 
+
+for i in range(len(data)): 
+    data[i] = float(data[i])
+
+stats[id] = {'max':max(data), 'min':min(data), 'mean':np.average(data), 'std':np.std(data), 'data': data} 
+
+print stats 
