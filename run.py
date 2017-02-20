78ce58ca866a8182044c0dcc4aaba7970eddf28c
dhanus <dhanus@mit.edu>
2017-02-19 21:26:07 -0500

    make run.py work for assorted data and buffer sizes

--- run.py
+++ run.py
@@ -1,12 +1,12 @@
 from subprocess import call
 
-buffer_sizes = [100, 1000, 10000, 100000]
-data_sizes = [1000, 10000, 100000, 1000000, 10000000] 
+buffer_sizes = ['100', '1000', '10000', '100000']
+data_sizes = ['1000', '10000', '100000', '1000000', '10000000'] 
 
-bs = str(buffer_sizes[0])
-ds = str(data_sizes[0])
-
-f = open("put_%s_%s.txt"%(ds, bs), "w")
-for i in range(10):
-    call(["./lsm", ds, bs], stdout=f)
+# collect times for each data and buffer size 
+for ds in data_sizes: 
+    for bs in buffer_sizes: 
+        f = open("data/put_%s_%s.txt"%(ds, bs), "w")
+        for i in range(50):
+            call(["./lsm", ds, bs], stdout=f)
 
