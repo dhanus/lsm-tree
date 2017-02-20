c2a822597b193ca910a0d6507b6736c0d24c85b4
dhanus <dhanus@mit.edu>
2017-02-20 12:29:56 -0500

    modify analysis to look at all items. and stop storing max and min due to errors

--- analyze.py
+++ analyze.py
@@ -2,14 +2,14 @@ import numpy as np
 import cPickle as pickle 
 
 actions = ['put'] 
-data_sizes = ['1000', '10000', '100000', '1000000', '10000000']
+data_sizes = ['1000', '10000', '100000', '1000000']
 buffer_sizes = ['100', '1000', '10000', '100000']  
 
 stats = {} 
 
-a = actions[0]
-ds = data_sizes[0]
-bs = buffer_sizes[0]
+# a = actions[0]
+# ds = data_sizes[0]
+# bs = buffer_sizes[0]
 
 for a in actions: 
     for ds in data_sizes: 
@@ -20,8 +20,9 @@ for a in actions:
 
             for i in range(len(data)): 
                 data[i] = float(data[i])
-
-                stats[id] = {'max':max(data), 'min':min(data), 'mean':np.average(data), 'std':np.std(data), 'data': data} 
+            
+            data = np.array(data)
+            stats[id] = {'mean':np.average(data), 'std':np.std(data), 'data': data} 
 
 pickle.dump(stats, open('stats.p','w')) 
 print stats 
