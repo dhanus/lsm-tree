be617b06bf99e0003c658e121a21dc74f24ae393
dhanus <dhanus@mit.edu>
2017-02-23 12:47:07 -0500

    running 1M does not work. Try 500K

--- run.py
+++ run.py
@@ -2,7 +2,8 @@ from subprocess import call
 
 
 buffer_sizes = ['100', '1000', '10000', '100000']
-data_sizes = ['1000', '10000', '100000', '1000000', '10000000'] 
+#data_sizes = ['1000', '10000', '100000', '1000000', '500000'] 
+data_sizes = ['500000'] 
 
 # collect times for each data and buffer size 
 for ds in data_sizes: 
