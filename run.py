6a5e391e2240171489b78d10d64c0b479cbb56b6
dhanus <dhanus@mit.edu>
2017-02-25 06:38:14 -0800

    set up file to collect unsorted data

--- run.py
+++ run.py
@@ -1,14 +1,14 @@
 from subprocess import call
 
 
+actions = ['put_sorted', 'put_unsorted'] 
 buffer_sizes = ['100', '1000', '10000', '100000']
-#data_sizes = ['1000', '10000', '100000', '1000000', '500000'] 
-data_sizes = ['500000'] 
+data_sizes = ['1000', '10000', '100000', '1000000', '1000000'] 
 
 # collect times for each data and buffer size 
 for ds in data_sizes: 
     for bs in buffer_sizes: 
-        f = open("data/put_sorted_%s_%s.txt"%(ds, bs), "w")
+        f = open("data/put_unsorted_%s_%s.txt"%(ds, bs), "w")
         for i in range(50):
             call(["./lsm", ds, bs], stdout=f)
 
