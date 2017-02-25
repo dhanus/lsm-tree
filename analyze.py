35fa15c712fe2718036b25b37b070ed8b9648452
dhanus <dhanus@mit.edu>
2017-02-25 07:47:03 -0800

    store data in np arrays

--- analyze.py
+++ analyze.py
@@ -1,4 +1,4 @@
-import matplotlib as plt 
+
 import numpy as np 
 import cPickle as pickle 
 
@@ -9,24 +9,35 @@ buffer_sizes = ['100', '1000', '10000', '100000']
 stats = {} 
 ids = [] 
 
-# a = actions[0]
+a = actions[0]
 # ds = data_sizes[0]
 # bs = buffer_sizes[0]
-
-for a in actions: 
-    for ds in data_sizes: 
-        for bs in buffer_sizes: 
-            id = '%s_%s_%s'%(a, ds, bs)
+means = np.zeros([len(data_sizes), len(buffer_sizes)]) 
+stds =  np.zeros([len(data_sizes), len(buffer_sizes)]) 
+for a in range(len(actions)): 
+    for d in range(len(data_sizes)):
+        bs_means =[]
+        bs_stds =[]
+        for b in range(len(buffer_sizes)): 
+            id = '%s_%s_%s'%(actions[a], data_sizes[d], buffer_sizes[b])
             ids.append(id)
-            f = open('data/%s.txt'%(id), 'r')
+            f = open('data/%s/%s.txt'%(actions[a], id), 'r')
             data = f.readline().split(',')[:-1] 
 
             for i in range(len(data)): 
                 data[i] = float(data[i])
             
             data = np.array(data)
-            stats[id] = {'mean':np.average(data), 'std':np.std(data), 'data': data} 
-
+            mean = np.average(data)
+            std = np.std(data)
+            means[d,b] = mean 
+            stds[d,b] = std 
+            stats[id] = {'mean': mean, 'std': std, 'data': data} 
+ 
 pickle.dump(stats, open('stats.p','w')) 
-
+pickle.dump(means, open('means.p','w')) 
+pickle.dump(stds, open('stds.p','w')) 
+print stats.keys() 
+print means 
+print stds 
 
