5d19c9ff18676152bf6bfbce31607510566b722c
dhanus <dhanus@mit.edu>
2017-05-11 13:04:26 -0400

    made changes to run file to test get

--- run.py
+++ run.py
@@ -1,14 +1,17 @@
 from subprocess import call
 
 
-actions = ['put_sorted', 'put_unsorted'] 
-buffer_sizes = ['100', '1000', '10000', '100000']
-data_sizes = ['1000', '10000', '100000', '1000000', '1000000'] 
+actions = ['put_sorted', 'put_unsorted', 'get_sorted'] 
+#buffer_sizes = ['100', '1000', '10000', '100000']
+#data_sizes = ['1000', '10000', '100000', '1000000']# '1000000'] 
 
+buffer_sizes =['100']
+data_sizes = ['1000']
+a = actions[2]
 # collect times for each data and buffer size 
 for ds in data_sizes: 
     for bs in buffer_sizes: 
-        f = open("data/put_unsorted_%s_%s.txt"%(ds, bs), "w")
-        for i in range(50):
+        f = open("data/%s/%s_%s_%s.txt"%(a,a,ds, bs), "w")
+        for i in range(1):
             call(["./lsm", ds, bs], stdout=f)
 
