c4649011b8d94bb2d5dc9446d2eecb4c58ff7bdc
dhanus <dhanus@mit.edu>
2017-05-19 10:01:50 -0400

    add comments to run.py

--- run.py
+++ run.py
@@ -5,13 +5,13 @@ actions = ['put_sorted', 'put_unsorted', 'get_sorted', 'get_unsorted', 'update_s
 buffer_sizes = ['100', '1000', '10000', '100000']
 data_sizes = ['1000', '10000', '100000', '1000000', '1000000'] 
 
-#buffer_sizes = ['100']
-#data_sizes = ['1000']
-a = actions[-1]
+## Select the action that you would like to test 
+a = actions[5]
 num_ops = '100'
 # collect times for each data and buffer size 
 for ds in data_sizes: 
     for bs in buffer_sizes: 
-        f = open("data/%s/%s_%s_%s.txt"%(a,a,ds, bs), "w")
-        call(["./lsm", ds, bs, num_ops], stdout=f)
+        for i in range(5):
+            f = open("data/%s/%s_%s_%s.txt"%(a,a,ds, bs), "w")
+            call(["./lsm", ds, bs, num_ops], stdout=f)
 
