9d3b9b57571c933138b3463836f050e3dbf177ca
dhanus <dhanus@mit.edu>
2017-07-23 18:48:52 -0700

    add docstrings to lsm.c

--- lsm.c
+++ lsm.c
@@ -10,6 +10,11 @@
 #include "lsm.h"
 
 void check_file_ret(FILE* f, int r){
+  /*Check return value of file pointer.
+    Args: 
+    f (FILE*): file pointer. 
+    r (int): file return value.
+  */  
   if(r == 0){ 
     if(ferror(f)){
       perror("ferror\n");
@@ -21,6 +26,13 @@ void check_file_ret(FILE* f, int r){
 }
 
 lsm* init_new_lsm(size_t buffer_size, bool sorted){
+  /*Initializes new lsm tree onject. 
+    Args:
+    buffer_size (size_t): size of in-memory buffer.
+    sorted (bool): if true data on disk will be sorted.
+    Returns: 
+    populated lsm object.
+  */ 
   lsm* tree;
   tree = malloc(sizeof(lsm));
   if(!tree){
@@ -43,11 +55,23 @@ lsm* init_new_lsm(size_t buffer_size, bool sorted){
 
 
 void destruct_lsm(lsm* tree){
+  /* Destruct LSM object.
+     Args: 
+     tree (lsm*): pointer to LSM object to destroy.
+  */  
   free(tree->block);
   free(tree);
 }
 
 void merge(node *whole, node *left,int left_size,node *right,int right_size){
+  /* Performs the merge step in merge sort. 
+     Args:
+     whole (node*): pointer to the node representing the merged array of nodes. 
+     left (node*): pointer to the left split of nodes to be merged. 
+     left_size (int): int representing the the size of the left split. 
+     right (node*): pointer to the right split of nodes to be merged. 
+     rightt_size (int): int representing the the size of the right split. 
+  */    
   int l, r, i;
   l = 0; r = 0; i = 0;
 
@@ -68,9 +92,13 @@ void merge(node *whole, node *left,int left_size,node *right,int right_size){
 
 
 void merge_sort(node *block, int n){
-  /* Given an unsorted char array containing nores , this returns
-     a char array sorted by key using merge sort */
-  /* Assumption: block will only be sorted when full */
+  /* Given an unsorted array of nodes, sorts by key.
+     Args: 
+     block (node*): pointer to a node representing the block to sort. 
+     n (int): int describing the size of the block to be sorted.
+ */
+
+  //Key assumption: block will only be sorted when full
   assert(block != NULL);
   if(n < 2){
     return;
@@ -97,6 +125,11 @@ void merge_sort(node *block, int n){
 
 
 nodei* search_buffer(const keyType* key, lsm* tree){
+  /* Searches in-memory buffer for a given key. 
+     Args: 
+     key (keyType*): key to search for. 
+     tree (lsm*): pointer to an lsm tree. 
+   */
   for (int i = 0; i < tree->next_empty; i++){
     if (tree->block[i].key == *key){
       nodei* nodei = malloc(sizeof(nodei));
@@ -111,6 +144,11 @@ nodei* search_buffer(const keyType* key, lsm* tree){
 }
 
 nodei* search_disk(const keyType* key, lsm* tree){
+  /* Searches disk for a given key. 
+     Args: 
+     key (keyType*): key to search for. 
+     tree (lsm*): pointer to an lsm tree.
+   */
   int r; 
   FILE* f = fopen("disk_storage.txt", "r");
   if(f == NULL){
@@ -145,6 +183,11 @@ nodei* search_disk(const keyType* key, lsm* tree){
 }
 
 node* get(const keyType key, lsm* tree){
+  /* Retrieves node associated with a given key. 
+     Args: 
+      key (keyType*): key to search for. 
+      tree (lsm*): pointer to an lsm tree.
+   */
   // search the buffer for this item
   nodei* ni = search_buffer(&key, tree);
   if(ni != NULL){
@@ -161,6 +204,10 @@ node* get(const keyType key, lsm* tree){
 }
 
 int write_to_disk(lsm* tree){
+  /* Write all data (in disk & in memory) to disk. 
+     Args: 
+     tree (lsm*): pointer to an lsm tree.
+   */
   node *complete_data = NULL;
   node *file_data = NULL;
   size_t num_elements = 0;
@@ -223,6 +270,11 @@ int write_to_disk(lsm* tree){
 
 
 int put(const keyType* key, const valType* val, lsm* tree){
+  /* Places a given key and value into the lsm tree as a node.
+     Args: 
+     key (const keyType*): key to place in node. 
+     val (const valType*): value to place in node. 
+   */
   int r = 0; 
   if(tree->next_empty == tree->block_size){
     // buffer is full and must be written to disk
@@ -238,6 +290,11 @@ int put(const keyType* key, const valType* val, lsm* tree){
 
 
 int delete(const keyType* key, lsm* tree){
+  /* Deletes the node cotnaining the given key.
+     Args:
+     key (const keyType*): key associated with node to delete. 
+     tree (lsm*): lsm object to delete it from. 
+  */ 
   int r = 0;
   nodei *ni = malloc(sizeof(nodei));
   // if the node is in the buffer
@@ -296,7 +353,12 @@ int delete(const keyType* key, lsm* tree){
 
 
 int update(const keyType* key, const valType* val, lsm* tree){
-  /* search buffer, search disk, update value  */
+  /* Searches buffer &  disk for key, updates associated value. 
+     Args:
+     key (const keyType*): pointer to key to search for. 
+     val (const valType*): pointer to val to update. 
+     tree (lsm*): pointer to lsm object to make update in. 
+  */
   int r; 
   nodei* ni = search_buffer(key, tree);
   //printf("index is: ni %d \n", ni->index);
@@ -316,7 +378,10 @@ int update(const keyType* key, const valType* val, lsm* tree){
 }
 
 void print_buffer_data(lsm* tree){
-  printf("printing buffer data\n");
+  /* Prints all data in buffer. 
+     Args: 
+     tree (lsm*): pointer to lsm associated with data. 
+   */
   for(int i = 0; i < tree->next_empty; i++){
     printf("key %i \n",tree->block[i].key);
     printf("value %i\n",tree->block[i].val);
@@ -324,6 +389,10 @@ void print_buffer_data(lsm* tree){
 }
 
 void print_disk_data(lsm* tree){
+  /* Prints all data on disk. 
+     Args: 
+     tree (lsm*): pointer to lsm associated with data. 
+   */
   printf("printing disk data\n");
   FILE* f = fopen(tree->disk1, "r"); 
   node *file_data;
